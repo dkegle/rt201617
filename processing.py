6d28b92f5ca52f8ad1bd1214ed5f5b850f3660c0
mtsch <matijacufar@gmail.com>
2017-05-31 11:00:55 +0200

    Added basic gudhi functionality

---
+++ processing.py
@@ -0,0 +1,43 @@
+import gudhi as gd
+
+class TextComplex:
+
+	def __init__(self, pts, name="", max_alpha_square=0.5):
+		""" Build Processor from list of points (list of lists of numbers)
+		"""
+		self.pts = pts
+		self.name = name
+		self.alpha = gd.AlphaComplex(points = pts)
+		self.max_alpha_square = 0.5
+
+		print("Building %s..." % name)
+		self.sxtree = self.alpha.create_simplex_tree(max_alpha_square = self.max_alpha_square)
+		print("Number of sx's: %d" % sxtree.num_simplices())
+
+		self.diag = sxtree.persistence()
+
+	def __repr__(self):
+		return "TextComplex(" + self.name + ")"
+
+	def plot_persistence_diagram(self):
+		""" Plot persistence diagram.
+		"""
+		gd.plot_persistence_diagram(self.diag)
+
+	def plot_persistence_barcode(self):
+		""" Plot barcode diagram.
+		"""
+		gd.plot_persistence_barcode(self.diag)
+
+	def bottleneck_distance_to(self, other, e=0):
+		""" Compute the bottleneck distance to antoher TextComplex.
+		"""
+		if not isinstance(other, TextComplex):
+			raise "Invalid paramter!"
+
+		return gd.bottleneck_distance(self.diag, other.diag, e)
+
+	def betti_numbers(self):
+		""" Return the betti numbers of TextComplex.
+		"""
+		return self.sxtree.betti_numbers()
